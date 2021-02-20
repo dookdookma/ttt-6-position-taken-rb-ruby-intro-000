099a00a83604ea9a3a47ec978304a88b82b61dcd
Luke Rollins <ldrollins@gmail.com>
2021-02-20 12:32:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
 
   taken = nil
-  if (board [index] == " " || board[index] == "" || board[index] == nil)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
     taken = false
   else
     taken = true
